c3670a164dfe54510cf99ddd8cb3db61705a6d0e
Niccolo Canestrari <niccolo.canestrari@gmail.com>
2023-12-14 19:32:25 +0100

    Update Forno.h

--- Forno.h
+++ Forno.h
@@ -10,29 +10,28 @@
 
 #include "Arduino.h"
 
-enum statoForno {
+enum Stato {
     SPENTO,
     ACCESO
 };
 
-
 class Forno {
-    const _max_temp = 300.0;
-    const _min_temp = 20.0;
-    double _temperatura = 0.0;
-    double _potenzaPercentuale;  //potenza del forno settata (in futuro dinamicamente dal PID)
-    double _riscaldamento;       //variazione temperatura causata dall'azione di riscaldamento
+    const double _max_temp = 300.0;
+    const double _min_temp = 20.0;
+    double _temperatura = 20.0;
+    double _potenzaPercentuale;  // potenza del forno settata (in futuro dinamicamente dal PID)
+    double _riscaldamento;       // variazione temperatura causata dall'azione di riscaldamento
     double _raffreddamento;      // variazione temperatura causata dall'azione di raffreddamento
-    double _tassoRiscaldamento;  //una caratteristica del forno, un k che definisce il tasso di riscaldamento. 
-    double _tassoRaffreddamento; //una caratteristica del forno, un k che definisce il tasso di raffreddamento. 
-    statoForno _stato = SPENTO;  // enum, acceso o spento
+    double _tassoRiscaldamento;  // una caratteristica del forno, un k che definisce il tasso di riscaldamento. 
+    double _tassoRaffreddamento; // una caratteristica del forno, un k che definisce il tasso di raffreddamento. 
+    Stato  _stato = SPENTO;      // enum, acceso o spento
 
 public:
-    Forno( double tassoRiscaldamento, double tassoRaffreddamento );  // costruttore
-    void aggiorna();                                                 // aggiorna la temperatura del forno
-    double temperatura();                                            // funzione che restituisce la temperatura attuale del forno
-    statoForno stato();                                              // funzione che restituisce lo stato (acceso/spento) del forno
-    void accendi();                                                  // metodo per accendere il forno
-    void spegni();                                                   // metodo per spegnere il forno
-    void impostaPotenzaPercentuale(double potenza);                  // metodo che imposta la potenza di riscaldamento del forno
+    Forno(double tassoRiscaldamento, double tassoRaffreddamento);  // costruttore
+    void aggiorna();                                               // aggiorna la temperatura del forno
+    double temperatura();                                          // metodo per ottenere la temperatura attuale del forno
+    Stato stato();                                                 // metodo per ottenere lo stato (acceso/spento) del forno
+    void accendi();                                                // metodo per accendere il forno
+    void spegni();                                                 // metodo per spegnere il forno
+    void impostaPotenzaPercentuale(double potenza);                // metodo che imposta la potenza di riscaldamento del forno
 };
